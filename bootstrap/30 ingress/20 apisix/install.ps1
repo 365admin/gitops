2fceb2e6900deb757478b1fb0e41af8f4c3b3bd6
Niels Gregers Johansen <niels@jumpto365.com>
2023-05-31 15:36:32 +0200

    more

--- bootstrap/30 ingress/20 apisix/install.ps1
+++ bootstrap/30 ingress/20 apisix/install.ps1
@@ -1,3 +1,5 @@
 # helm repo add apisix https://charts.apiseven.com
-helm upgrade -f $PSScriptRoot/values.yaml apisix apisix/apisix --set gateway.type=LoadBalancer --set dashboard.enabled=true --set ingress-controller.enabled=true --set ingress-controller.config.apisix.serviceNamespace=gateway --set gateway.tls.enabled=true
+#helm install -f $PSScriptRoot/values.yaml apisix apisix/apisix --set gateway.type=LoadBalancer --set dashboard.enabled=true --set ingress-controller.enabled=false --set ingress-controller.config.apisix.serviceNamespace=gateway
+helm install -f $PSScriptRoot/values.yaml apisix apisix/apisix --set dashboard.enabled=true --set ingress-controller.enabled=false --set ingress-controller.config.apisix.serviceNamespace=gateway
+
 # helm uninstall apisix
--- bootstrap/30 ingress/20 certmgr/install.ps1
+++
@@ -1,3 +0,0 @@
-#helm repo add jetstack https://charts.jetstack.io
-#helm repo update
-helm install -f $PSScriptRoot/values.yaml cert-manager jetstack/cert-manager --version v1.12.0 --set installCRDs=true
